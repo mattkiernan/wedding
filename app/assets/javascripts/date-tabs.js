177dfa10e678cd63946c7563642ac0dfbbe13f33
mattkiernan <matt.kiernan@gmail.com>
2015-05-25 07:47:59 -0500

    Add lightboxes

--- app/assets/javascripts/date-tabs.js
+++ app/assets/javascripts/date-tabs.js
@@ -2,16 +2,26 @@ $(document).ready(function(){
 
   $('ul.tabs li').click(function(){
     var tab_id = $(this).attr('data-tab');
-    tab_set = $(this).closest('ul');
-    tab_set_li = tab_set.find('li')
-    all_content = tab_set.closest("div").children("div");
+    var tab_set = $(this).closest('ul');
+    var tab_set_li = tab_set.find('li');
+    var all_content = tab_set.closest("div").children("div");
 
-  $(tab_set_li).removeClass('current');
-  $(all_content).removeClass('current');
+    $(tab_set_li).removeClass('current');
+    $(all_content).removeClass('current');
 
-  $(this).addClass('current');
-  $("#"+tab_id).addClass('current');
-  })
+    $("#"+tab_id).fadeIn(1000, function(){
+      $("#"+tab_id).addClass('current');
+      $(".liberty-section-header").fadeOut("fast");
+      $(".liberty-tabs").fadeOut("fast");
+    });
+  });
 
-})
-;
+  $(".x-circle").click(function(){
+    var x_tab_id = $(this).attr('data-tab');
+    $("#"+x_tab_id).fadeOut(1000, function(){
+      $("#"+x_tab_id).removeClass('current');
+      $(".liberty-section-header").fadeIn("fast");
+      $(".liberty-tabs").fadeIn("fast");
+    });
+  });
+});
