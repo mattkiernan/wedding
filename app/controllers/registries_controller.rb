1fe63ef0b3d8a6861de7fa1623060d4c9fb479f8
mattkiernan <matt.kiernan@gmail.com>
2015-07-14 07:51:35 -0400

    Replace registry

---
+++ app/controllers/registries_controller.rb
@@ -0,0 +1,6 @@
+class RegistriesController < ApplicationController
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resource :homepage, only: [:show]
   resource :rsvp, only: [:show]
+  resource :registry, only: [:show]
   root "homepages#show"
 end
